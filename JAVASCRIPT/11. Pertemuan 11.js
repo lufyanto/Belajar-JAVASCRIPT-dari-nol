8caf2f50404d670f3364ba0a64053ec809cad2ed
Lufyanto Eka Fahrezi <lufyantoekalearning@gmail.com>
2026-08-07 11:29:11 +0700

    PERTEMUAN 11 - PENUTUP

---
+++ JAVASCRIPT/11. Pertemuan 11.js
@@ -0,0 +1,18 @@
+function tambah(a,b){
+    hasil = a + b
+    console.log(hasil)
+    return hasil
+}
+
+tambah(3,5)
+tambah("3",5)
+
+
+/** Ini adalah JSDOC comment
+ * TOLONG BAPAK BAPAK KODER INI DIATUR
+ * @param {number} a - ini angka pertama
+ * @param {number} b - ini angka kedua
+ * @return {number} hasil  - ini mengembalikan hasil
+ * 
+ */
+
