76d34fda5706d01b6fe46fc01ba89dc109b56929
Lufyanto Eka Fahrezi <lufyantoekalearning@gmail.com>
2026-08-18 11:47:04 +0700

    Tambahan

--- Materi/9. PolaBintang.js
+++ Materi/9. PolaBintang.js
@@ -16,3 +16,35 @@ for (let x = 1; x <= n ; x++){
     let spasi = " ";
     console.log(spasi.repeat(n-x) + bintang.repeat(x * 2 -1));
 }
+
+console.log("1. POLA SEGITIGA SIKU-SIKU");
+
+let tinggi = 5;
+
+for(let i = 1; i <= tinggi; i++ ){
+    let baris = "";
+    for (let j = 1; j <= i; j++){
+        baris += "*";
+    }
+    console.log(baris);
+}
+
+console.log("2. POLA PERSEGI BINTANG");
+
+let ukuran = 5;
+
+for (let i = 0; i < ukuran; i++){
+    let baris = "";
+    for (let j = 0; j < ukuran; j++){
+        baris += "*  ";
+    }
+    console.log(baris);
+}
+
+console.log("3. POLA REPEAT")
+
+let n = 10;
+
+for (let i = 1; i <= n; i++){
+    console.log("*".repeat(i));
+}
